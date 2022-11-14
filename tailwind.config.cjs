352f7c96a2c72ae278175a3ee2f8c5316a6f137b
M Haidar Hanif <git@mhaidarhanif.com>
2022-11-14 14:58:09 +0700

    feat: change primary secondary colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,8 +6,8 @@ module.exports = {
 	theme: {
 		extend: {
 			colors: {
-				primary: colors.blue,
-				secondary: colors.pink,
+				primary: colors.cyan,
+				secondary: colors.sky,
 			},
 			fontFamily: {
 				sans: ["'InterVariable'", ...defaultTheme.fontFamily.sans],
