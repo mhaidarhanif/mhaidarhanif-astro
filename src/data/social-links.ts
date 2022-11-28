6e28ab369c2979ae16d9bbc1298fab373644692c
M Haidar Hanif <git@mhaidarhanif.com>
2022-11-29 02:37:50 +0700

    refactor: social links from data
    also add target and rel

---
+++ src/data/social-links.ts
@@ -0,0 +1,37 @@
+export const dataSocialLinks = [
+	{
+		label: 'GitHub',
+		href: 'https://github.com/mhaidarhanif',
+		iconName: 'tabler:brand-github',
+	},
+	{
+		label: 'LinkedIn',
+		href: 'https://linkedin.com/in/mhaidarhanif',
+		iconName: 'tabler:brand-linkedin',
+	},
+	{
+		label: 'YouTube',
+		href: 'https://youtube.com/mhaidarhanif',
+		iconName: 'tabler:brand-youtube',
+	},
+	{
+		label: 'Twitter',
+		href: 'https://twitter.com/mhaidarhanif',
+		iconName: 'tabler:brand-twitter',
+	},
+	{
+		label: 'Facebook',
+		href: 'https://facebook.com/mhaidarhanif',
+		iconName: 'tabler:brand-facebook',
+	},
+	{
+		label: 'Instagram',
+		href: 'https://instagram.com/mhaidarhanif_',
+		iconName: 'tabler:brand-instagram',
+	},
+	{
+		label: 'RSS',
+		href: '/rss.xml',
+		iconName: 'tabler:rss',
+	},
+];
