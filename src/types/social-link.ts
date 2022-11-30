987497a30dca08e0d315ecef2212daa19d2096e8
M Haidar Hanif <git@mhaidarhanif.com>
2022-12-01 02:32:34 +0700

    feat: brand colors with slug and hover states

--- src/types/social-link.ts
+++ src/types/social-link.ts
@@ -2,8 +2,9 @@ export type SocialLinks = SocialLink[];
 
 export type SocialLink = {
 	label: string;
+	slug?: string;
+	colorCode?: string;
 	href: string;
 	iconName: string;
-	color?: string;
 	inHeader?: boolean;
 };
